6410a36f7958b0f3cd9967eacde508bb8c031b33
KELVIN WELLINGTON RIBEIRO MORA <39336809+KELVIMW@users.noreply.github.com>
2020-11-28 11:48:51 -0300

    Add files via upload

---
+++ front/src/Produto.js
@@ -0,0 +1,10 @@
+import React from "react"
+import { Col, Row } from "reactstrap"
+
+export default function Produto() {
+    return (
+        <Row>
+            <Col>Lista de produtos</Col>
+        </Row>
+    )
+}
